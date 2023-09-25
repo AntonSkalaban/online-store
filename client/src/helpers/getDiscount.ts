78416b901a1f0450364bbcf26b4e03ba7bce39f4
Anton Skalaban <antonskalaban@MacBook-Air-Anton.local>
2023-09-25 09:55:42 +0300

    fix: fix discount calculation

--- client/src/helpers/getDiscount.ts
+++ client/src/helpers/getDiscount.ts
@@ -1,3 +1,5 @@
 export const getDiscount = (amount: number, discount: number) => {
-  return +(amount - (1 - discount / 100)).toFixed(2);
+  const discountAmount = (amount * discount) / 100;
+  const discountedPrice = amount - discountAmount;
+  return +discountedPrice.toFixed(2);
 };
